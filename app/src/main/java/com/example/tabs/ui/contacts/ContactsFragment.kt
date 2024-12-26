62ac69235d8284dcbcf222cdd05a73647cc22094
SeeWhy <seewhy@unist.ac.kr>
2024-12-26 16:52:58 +0900

    name change to contacts

--- app/src/main/java/com/example/tabs/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/com/example/tabs/ui/contacts/ContactsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tabs.ui.dashboard
+package com.example.tabs.ui.contacts
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.tabs.databinding.FragmentDashboardBinding
+import com.example.tabs.databinding.FragmentContactsBinding
 
-class DashboardFragment : Fragment() {
+class ContactsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentContactsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val contactsViewModel =
+            ViewModelProvider(this).get(ContactsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentContactsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textContacts
+        contactsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/tabs/ui/contacts/ContactsViewModel.kt
+++ app/src/main/java/com/example/tabs/ui/contacts/ContactsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.tabs.ui.dashboard
+package com.example.tabs.ui.contacts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ContactsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is contacts Fragment"
     }
     val text: LiveData<String> = _text
 }
